7ecd38de034fb1bae4e6e0871c6cd089d787ccf8
unknown <jn.zezin@gmail.com>
2026-04-22 21:45:33 -0300

    update

--- database/schema.sql
+++ database/schema.sql
@@ -1,24 +1,24 @@
 CREATE DATABASE advocacia;
 USE advocacia;
-CREATE TABLE clientes (
+
+CREATE TABLE cadastroAdvogado (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(14) UNIQUE NOT NULL,
+    nome_adv VARCHAR(100) NOT NULL,
     email VARCHAR(100),
-    telefone VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE processos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_processo INT AUTO_INCREMENT PRIMARY KEY,
     numero_processo VARCHAR(50) UNIQUE NOT NULL,
-    tipo_acao VARCHAR(50),
-    status ENUM('Em andamento', 'Concluído', 'Arquivado', 'Pendente') DEFAULT 'Em andamento',
-    data_protocolo DATE,
-    cliente_id INT,
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+    nome_cliente VARCHAR(50) NOT NULL,
+    tipo_acao VARCHAR(50) NOT NULL,
+    status_processo ENUM('Em andamento', 'Concluído', 'Arquivado', 'Pendente') DEFAULT 'Em andamento',
+    data_protocolo DATE NOT NULL,
+    cpf_cliente VARCHAR(11) NOT NULL,
+    adv_id INT NOT NULL,
+    FOREIGN KEY (adv_id) REFERENCES cadastroAdvogado(id)
+    
 );
-USE advocacia;
 
-INSERT INTO clientes (nome, cpf) VALUES
-INSERT INTO processos (numero_processo, tipo_acao, status, data_protocolo, cliente_id) VALUES
+USE advocacia;
